065512dece7c33a92a9954e895a1c18cc0384db6
Harshil Lakhani <harshillakhani2264@gmail.com>
2024-06-11 00:03:45 +0530

    added thumbnail instead of direct playing video in videoModel.

---
+++ lib/features/home/screen/video_player_screen.dart
@@ -0,0 +1,64 @@
+import 'package:chewie/chewie.dart';
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+class VideoPlayerScreen extends StatefulWidget {
+  const VideoPlayerScreen({super.key, required this.videoUrl});
+
+  final String videoUrl;
+
+  @override
+  _VideoPlayerScreenState createState() => _VideoPlayerScreenState();
+}
+
+class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
+  late VideoPlayerController videoPlayerController;
+  ChewieController? chewieController;
+
+  @override
+  void initState() {
+    super.initState();
+    initializePlayer(widget.videoUrl);
+  }
+
+  Future<void> initializePlayer(String url) async {
+    videoPlayerController = VideoPlayerController.network(url);
+
+    await Future.wait([videoPlayerController.initialize()]);
+    chewieController = ChewieController(
+      videoPlayerController: videoPlayerController,
+      autoPlay: true,
+      looping: true,
+      materialProgressColors: ChewieProgressColors(
+        playedColor: Colors.red,
+        handleColor: Colors.white,
+        backgroundColor: Colors.yellow,
+        bufferedColor: Colors.grey,
+      ),
+      autoInitialize: true,
+    );
+    setState(() {});
+  }
+
+  @override
+  void dispose() {
+    videoPlayerController.dispose();
+    chewieController?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Video Player'),
+      ),
+      body: Center(
+        child: (chewieController != null &&
+            chewieController!.videoPlayerController.value.isInitialized)
+            ? Chewie(controller: chewieController!)
+            : const CircularProgressIndicator(),
+      ),
+    );
+  }
+}
